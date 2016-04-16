84e550311661d26e92c513af17694ecb6253b93f
waffleboy <A0130737@nus.edu.sg>
2016-04-16 20:14:16 +0800

    Added ensemble option.

--- DMC2016.py
+++ DMC2016.py
@@ -9,7 +9,7 @@ import pandas as pd,numpy as np
 from sklearn import metrics,cross_validation
 from sklearn.grid_search import GridSearchCV
 from sklearn.preprocessing import LabelEncoder,Imputer
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier,ExtraTreesClassifier
 from sklearn.cross_validation import StratifiedShuffleSplit,train_test_split
 import xgboost as xgb
 
@@ -103,38 +103,7 @@ Output:
 def stratifiedSampleGenerator(dataset,target,subsample_size=0.1):
     X_fit,X_eval,y_fit,y_eval= train_test_split(dataset,target,test_size=subsample_size,stratify=target)
     return X_eval.reset_index(drop=True),y_eval.reset_index(drop=True)
-#    print('Generating stratified sample of size ' + str(round(len(dataset)*subsample_size,2)))
-#    dic={}
-#    indexes = np.array([])
-#    # find number of classes in sample
-#    for label in target.unique(): 
-#         labelSize = len(target[target==label]) 
-#         dic[label] = int(labelSize * subsample_size)
-#    # make a dataset of size sizeSample with ratio of classes in dic
-#    for label in dic:
-#        classIndex = target[target==label].index #obtain indexes of class
-#        counts = dic[label]   #get number of times class occurs
-#        newIndex = np.random.choice(classIndex,counts,replace=False)
-#        indexes = np.concatenate((indexes,newIndex),axis=0)
-#        
-#    indexes = indexes.astype(int)
-#    sampleData = dataset.ix[indexes]
-#    sampleData.drop('index',inplace=True,axis=1) #cause ix generates indexes as a column ffs
-#    sampleTarget = target[indexes]
-#    sampleData['returnQuantity'] = sampleTarget
-#    sampleData.reset_index(inplace=True)
-#    sampleData = sampleData.iloc[np.random.permutation(len(sampleData))] #shuffle dataset
-#    sampleData.drop('index',inplace=True,axis=1)
-#    sampleData.reset_index(inplace=True)
-#    
-#    newDataset = sampleData.drop('returnQuantity',axis=1)
-#    newTarget = sampleData['returnQuantity']
-#    if newDataset['sizeCode'].ftype != 'int32:dense':
-#        newDataset['sizeCode'] = newDataset['sizeCode'].astype(int)
-#        
-#    return newDataset,newTarget
-            
-        
+
 ###################################################
 #                   Models                        #
 ###################################################
@@ -146,8 +115,15 @@ def xgBoost():
     return clf
     
 def randomForest():
-    clf = RandomForestClassifier(max_depth=8, n_estimators=200,n_jobs=8,random_state=1)
+    clf = RandomForestClassifier(max_depth=8, n_estimators=200,n_jobs=8,random_state=1,
+                                 class_weight={0:2})
     return clf
+
+def extraTrees():
+    clf = ExtraTreesClassifier(max_depth=8, n_estimators=200,n_jobs=8,random_state=1,
+                               class_weight = {1:2})
+    return clf
+    
 
 ###################################################
 #              Optimize Models                    #
@@ -180,16 +156,22 @@ Input:
 2) <pf df> target: Pandas 1D dataframe of target labels
 3) <List or classifier> clfs: List of classifiers of single classifier
 4) <Boolean> cross_val: if True, use cross validation. else, normal train test split.
+5) <Boolean> ensemble: if True, ensemble all the classifiers you used. Else, no
+
+**Currently ensemble only works if cross_val == False.
+
 Output:
 1) None. Prints 5 fold cross val score, confusion matrix, and competition metric
 for all classifiers. 
 """
-def accuracyChecker(dataset,target,clfs,cross_val):
+def accuracyChecker(dataset,target,clfs,cross_val,ensemble):
     print('Beginning evaluation of models...')
     if type(clfs) != list:
         clfs = [clfs]
     if cross_val == False:
         trainx,testx,trainy,testy = train_test_split(dataset,target,test_size=0.3) #70 - 30 split
+    
+    predictions = []
     
     def errorScaler(error):
         global datasetSize
@@ -201,6 +183,7 @@ def accuracyChecker(dataset,target,clfs,cross_val):
         if name == 'XGBClassifier' and cross_val == True:
             XGBChecker(dataset,target,classifier)
             continue
+        
         if cross_val == True: #if cross val, do this. else, use train test split.
             print('******** '+name+' ********')
             predicted = cross_validation.cross_val_predict(classifier,dataset,target,cv=5)
@@ -209,6 +192,7 @@ def accuracyChecker(dataset,target,clfs,cross_val):
             error = computeError(predicted,target)
             print(name + 'Competition metric score : '+str(error))
             print(name + 'Competition metric score adjusted for train size: '+str(errorScaler(error)))
+            
         else:
             if name == 'XGBClassifier':
                 classifier.fit(trainx,trainy, early_stopping_rounds=25, 
@@ -217,11 +201,26 @@ def accuracyChecker(dataset,target,clfs,cross_val):
                 classifier.fit(trainx,trainy)
                 
             pred = classifier.predict(testx)
+            if ensemble:
+                predictions.append(pred)
+        
             print('Test data accuracy for '+name+': '+ str(classifier.score(testx,testy)))
             print(metrics.confusion_matrix(testy,pred,labels=[0,1,2,3,4,5]))
             error = computeError(pred,testy)
             print(name + ' Competition metric score : '+str(error))
             print(name + 'Competition metric score adjusted for train size: '+str(errorScaler(error)))
+            
+    if ensemble and cross_val == False:
+        predictions = np.array(predictions) #transpose it
+        predictions = predictions.T
+        clf = ExtraTreesClassifier(max_depth = 5,n_jobs=8)
+        predicted = cross_validation.cross_val_predict(clf,predictions,testy,cv=5)
+        print('5 fold cross val accuracy for ensemble '+str( round(metrics.accuracy_score(testy,predicted),2) ))
+        print(metrics.confusion_matrix(testy,predicted,labels=[0,1,2,3,4,5]))
+        error = computeError(predicted,testy)
+        print('Ensemble Competition metric score : '+str(error))
+        print('Ensemble Competition metric score adjusted for train size: '+str(errorScaler(error)))
+    
 
 """
 Function made to specifically handle xgboost. works similar to accuracyChecker.
@@ -253,8 +252,8 @@ def XGBChecker(dataset,target,classifier):
     print('Xgboost 5 fold cv: '+str(cvList))
     print('Average CV Score: '+ str(np.mean(cvList)))
     print(metrics.confusion_matrix(newTarget,predicted,labels=[0,1,2,3,4,5]))
-    print('Competition metric score : '+str(computeError(predicted,newTarget)))
-    
+    print('Competition metric score : '+str(computeError(predicted,newTarget)))        
+
 """
 Input:
 1) <PD DF> predicted: pandas df of predicted labels
@@ -275,8 +274,8 @@ def run():
     print('Processed data. Splitting..')
     dataset,target = splitDatasetTarget(train)
     dataset,target = stratifiedSampleGenerator(dataset,target,subsample_size=0.1)
-    clfs = [xgBoost(),randomForest()]
-    accuracyChecker(dataset,target,clfs,False)
+    clfs = [xgBoost(),randomForest(),extraTrees()]
+    accuracyChecker(dataset,target,clfs,False,True) 
     
     
 if __name__ == '__main__':
