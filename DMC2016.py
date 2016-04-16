b3e4dfb6b55c473b91038760e41ddb7f16834e5c
waffleboy <A0130737@nus.edu.sg>
2016-04-17 00:35:53 +0800

    added write to csv function

--- DMC2016.py
+++ DMC2016.py
@@ -4,7 +4,7 @@ Created on Mon Apr 11 10:22:37 2016
 
 @author: Thiru
 """
-
+import os,csv
 import pandas as pd,numpy as np
 from sklearn import metrics,cross_validation
 from sklearn.grid_search import GridSearchCV
@@ -12,7 +12,6 @@ from sklearn.preprocessing import LabelEncoder,Imputer
 from sklearn.ensemble import RandomForestClassifier,ExtraTreesClassifier
 from sklearn.cross_validation import StratifiedShuffleSplit,train_test_split
 import xgboost as xgb
-
 
 ###################################################
 #              Preprocessing Methods              #
@@ -110,18 +109,18 @@ def stratifiedSampleGenerator(dataset,target,subsample_size=0.1):
 
 ##Dont use this for accuracyChecker. Ran 1+ hr and didnt stop.
 def xgBoost():
-    clf = xgb.XGBClassifier(max_depth = 8,n_estimators=200,nthread=8,seed=1,silent=1,
-                            objective= 'multi:softmax',learning_rate=0.2,subsample=0.9)
+    clf = xgb.XGBClassifier(max_depth = 8,n_estimators=250,nthread=8,seed=1,silent=1,
+                            objective= 'multi:softmax',learning_rate=0.1,subsample=0.9)
     return clf
     
 def randomForest():
-    clf = RandomForestClassifier(max_depth=8, n_estimators=200,n_jobs=8,random_state=1,
-                                 class_weight={0:2})
+    clf = RandomForestClassifier(max_depth=8, n_estimators=300,n_jobs=8,random_state=1,
+                                 class_weight={0:2,1:1})
     return clf
 
 def extraTrees():
-    clf = ExtraTreesClassifier(max_depth=8, n_estimators=200,n_jobs=8,random_state=1,
-                               class_weight = {1:2})
+    clf = ExtraTreesClassifier(max_depth=8, n_estimators=300,n_jobs=8,random_state=1,
+                               class_weight = {0:1,1:2})
     return clf
     
 
@@ -157,6 +156,7 @@ Input:
 3) <List or classifier> clfs: List of classifiers of single classifier
 4) <Boolean> cross_val: if True, use cross validation. else, normal train test split.
 5) <Boolean> ensemble: if True, ensemble all the classifiers you used. Else, no
+6) <Boolean> record: if True, will record to CSV.
 
 **Currently ensemble only works if cross_val == False.
 
@@ -164,15 +164,16 @@ Output:
 1) None. Prints 5 fold cross val score, confusion matrix, and competition metric
 for all classifiers. 
 """
-def accuracyChecker(dataset,target,clfs,cross_val,ensemble):
+def accuracyChecker(dataset,target,clfs,cross_val,ensemble,record):
     print('Beginning evaluation of models...')
+    #if one classifier, make it into lst so not to break function
     if type(clfs) != list:
-        clfs = [clfs]
+        clfs = [clfs] 
+    #if not cross val, split now to save memory.
     if cross_val == False:
         trainx,testx,trainy,testy = train_test_split(dataset,target,test_size=0.3) #70 - 30 split
-    
-    predictions = []
-    
+    predictions = [] 
+    # function to show error wrt sample size of data
     def errorScaler(error):
         global datasetSize
         return (error*datasetSize) / len(dataset)
@@ -193,34 +194,60 @@ def accuracyChecker(dataset,target,clfs,cross_val,ensemble):
             print(name + 'Competition metric score : '+str(error))
             print(name + 'Competition metric score adjusted for train size: '+str(errorScaler(error)))
             
-        else:
+        else: #use train test split
+            #special fit for xgbclassifier
             if name == 'XGBClassifier':
                 classifier.fit(trainx,trainy, early_stopping_rounds=25, 
                        eval_metric="merror", eval_set=[(testx, testy)])
             else:
                 classifier.fit(trainx,trainy)
-                
             pred = classifier.predict(testx)
-            if ensemble:
+            if ensemble: #if ensemble, append to pred to use later
                 predictions.append(pred)
-        
-            print('Test data accuracy for '+name+': '+ str(classifier.score(testx,testy)))
-            print(metrics.confusion_matrix(testy,pred,labels=[0,1,2,3,4,5]))
+            #calculate metrics
+            testAccuracy = classifier.score(testx,testy)
+            confMat = metrics.confusion_matrix(testy,pred,labels=[0,1,2,3,4,5])
             error = computeError(pred,testy)
+            scaledError = errorScaler(error)
+            print('Test data accuracy for '+name+': '+ str(testAccuracy))
+            print(confMat)
             print(name + ' Competition metric score : '+str(error))
-            print(name + 'Competition metric score adjusted for train size: '+str(errorScaler(error)))
-            
-    if ensemble and cross_val == False:
+            print(name + 'Competition metric score adjusted for train size: '+str(scaledError))
+            if record:
+                params = classifier.get_params()
+                dataSize = len(testy)
+                writeToCSV(name,params,cross_val,dataSize,testAccuracy,confMat,error,scaledError)
+                
+     #if ensemble, do ensemble stuff       
+    if ensemble and len(clfs) >= 2 and cross_val == False:
         predictions = np.array(predictions) #transpose it
         predictions = predictions.T
-        clf = ExtraTreesClassifier(max_depth = 5,n_jobs=8)
+        clf = ExtraTreesClassifier(max_depth = 5,n_jobs=8,n_estimators=100)
         predicted = cross_validation.cross_val_predict(clf,predictions,testy,cv=5)
-        print('5 fold cross val accuracy for ensemble '+str( round(metrics.accuracy_score(testy,predicted),2) ))
-        print(metrics.confusion_matrix(testy,predicted,labels=[0,1,2,3,4,5]))
+        testAccuracy = round(metrics.accuracy_score(testy,predicted),2)
+        confMat = metrics.confusion_matrix(testy,predicted,labels=[0,1,2,3,4,5])
         error = computeError(predicted,testy)
+        scaledError= errorScaler(error)
+        
+        print('5 fold cross val accuracy for ensemble '+str(testAccuracy))
+        print(confMat)
         print('Ensemble Competition metric score : '+str(error))
-        print('Ensemble Competition metric score adjusted for train size: '+str(errorScaler(error)))
-    
+        print('Ensemble Competition metric score adjusted for train size: '+str(scaledError))
+        if record:
+            params = classifier.get_params()
+            dataSize = len(dataset)
+            writeToCSV('Ensemble',params,True,dataSize,testAccuracy,confMat,error,scaledError)
+
+#Function to write inputs to CSV        
+def writeToCSV(name,params,cross_val,size,testAccuracy,confMat,error,scaledError):
+    fileName = 'resultsCSV_Thiru.csv'
+    if os.path.isfile(fileName) == False:
+        with open(fileName,'w',newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(['Classifier','Params','Cross Val','Sample_Size','Accuracy','conf_Matrix','Competition_Error','Scaled_Error'])
+    with open(fileName,'a',newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([name,params,cross_val,size,testAccuracy,confMat,error,scaledError])
 
 """
 Function made to specifically handle xgboost. works similar to accuracyChecker.
@@ -268,15 +295,14 @@ def computeError(predicted,target):
 def run():
     train = pd.read_csv('E:/Git/DMC2016/thirufiles/orders_train.csv',sep=';')
     # train = pd.read_csv('/home/andre/workshop/dmc2016/andrefiles/orders_train.csv',sep=';')
-    train = preprocess(train,False)
+    train = preprocess(train,False) #False = dont use imputation
     global datasetSize
     datasetSize = len(train)
     print('Processed data. Splitting..')
     dataset,target = splitDatasetTarget(train)
     dataset,target = stratifiedSampleGenerator(dataset,target,subsample_size=0.1)
     clfs = [xgBoost(),randomForest(),extraTrees()]
-    accuracyChecker(dataset,target,clfs,False,True) 
-    
+    accuracyChecker(dataset,target,clfs,False,True,True) # Dont use CV, Yes ensemble, Yes Record. 
     
 if __name__ == '__main__':
 	run()
